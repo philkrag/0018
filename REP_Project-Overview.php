d156c364bf182c8cc5006b2a3608baf32d2bcaca
Phillip Kraguljac <37465931+philkrag@users.noreply.github.com>
2020-10-23 11:39:45 +1030

    Add files via upload

---
+++ REP_Project-Overview.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<?php
+// /////////////////////////////////////////////////////////////////////// COPYRIGHT NOTICE
+
+// This file is part of OS-Management-Portal.
+
+// OS-Management-Portal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// OS-Management-Portal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with OS-Management-Portal.  If not, see <http://www.gnu.org/licenses/>.
+
+// /////////////////////////////////////////////////////////////////////// COPYRIGHT NOTICE
+
+// /////////////////////////////////////////////////////////////////////// VERSION CONTROL
+
+// PAGE CREATED BY: Phillip Kraguljac
+// PAGE CREATED DATE: 2020-09-21
+
+// DATE   		|| NAME 					|| MODIFICATION
+// 2020-09-21 	|| Phillip Kraguljac 		|| v1.0.
+// 2020-10-07 	|| Phillip Kraguljac 		|| v1.1.
+// 2020-10-21 	|| Phillip Kraguljac 		|| v1.1.
+
+// /////////////////////////////////////////////////////////////////////// VERSION CONTROL
+?>
+
+<?php $Heading_Index = "1-1"; ?>
+
+<html>
+
+<div id="Main_Body">
+
+<head>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/Formats/Header_Basic.php';?>
+
+<title><?php echo "[".$Heading_Index."]"; ?> <?php echo date("Y-m-d"); ?> Project Overview <?php echo "ID:".$_GET['ID']; ?></title>
+</head>
+<body>
+
+<?php if(isset($_GET['ID'])){$Item_ID = Basic_Filter_Input($_GET['ID']);}else{$Item_ID = null;} ?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['New_Page_At_Print'] = false;
+$Display_Array['Table_Major_Heading'] = "PROJECT OVERVIEW";
+$Display_Array['Table_Minor_Heading'] = "Details";
+$Display_Array['Display_Items'] = array("ID",
+"Started Date",
+"Project Owner",
+"Description",
+"Internal Priority",
+"Scope",
+"Stage",
+"Status",
+"Completed Date"
+);
+$Display_Array['Column_Width'] = array("300px", "*" );
+$Display_Array['Item_Links'] = "REC_Projects.php";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `reg_projects` ";
+$Display_Array['MySQL_Filter'] = "WHERE `ID` = ".$Display_Array['ID']." ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "LIMIT 1";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_Details_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['New_Page_At_Print'] = false;
+$Display_Array['Table_Major_Heading'] = "OBJECTIVE(S)";
+$Display_Array['Table_Minor_Heading'] = "...";
+$Display_Array['Display_Items'] = array("ID", "Description");
+$Display_Array['Column_Width'] = array("50px", "*");
+$Display_Array['Item_Links'] = "";
+$Display_Array['New_Link_Reference'] = "";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `rec_objectives` ";
+$Display_Array['MySQL_Filter'] = "WHERE `Project ID` = ".$Item_ID." AND (`Deleted Date` IS NULL OR `Deleted Date` = '".date("Y-m-d")."') ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_List_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['New_Page_At_Print'] = false;
+$Display_Array['Table_Major_Heading'] = "DELIVERABLE(S)";
+$Display_Array['Table_Minor_Heading'] = "...";
+$Display_Array['Display_Items'] = array("ID", "Description");
+$Display_Array['Column_Width'] = array("50px", "*");
+$Display_Array['Item_Links'] = "";
+$Display_Array['New_Link_Reference'] = "";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `rec_deliverables` ";
+$Display_Array['MySQL_Filter'] = "WHERE `Project ID` = ".$Item_ID." AND (`Deleted Date` IS NULL OR `Deleted Date` = '".date("Y-m-d")."') ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_List_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['New_Page_At_Print'] = false;
+$Display_Array['Table_Major_Heading'] = "OBSTACLE(S)";
+$Display_Array['Table_Minor_Heading'] = "...";
+$Display_Array['Display_Items'] = array("ID", "Description");
+$Display_Array['Column_Width'] = array("50px", "*");
+$Display_Array['Item_Links'] = "";
+$Display_Array['New_Link_Reference'] = "";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `rec_obstacles` ";
+$Display_Array['MySQL_Filter'] = "WHERE `Project ID` = ".$Item_ID." AND (`Deleted Date` IS NULL OR `Deleted Date` = '".date("Y-m-d")."') ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_List_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['New_Page_At_Print'] = false;
+$Display_Array['Table_Major_Heading'] = "KEY SUCCESS CRITERIA(S)";
+$Display_Array['Table_Minor_Heading'] = "...";
+$Display_Array['Display_Items'] = array("ID", "Description");
+$Display_Array['Column_Width'] = array("50px", "*");
+$Display_Array['Item_Links'] = "";
+$Display_Array['New_Link_Reference'] = "";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `rec_key-success-criteria` ";
+$Display_Array['MySQL_Filter'] = "WHERE `Project ID` = ".$Item_ID." AND (`Deleted Date` IS NULL OR `Deleted Date` = '".date("Y-m-d")."') ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_List_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['New_Page_At_Print'] = false;
+$Display_Array['Table_Major_Heading'] = "ASSUMPTION(S)";
+$Display_Array['Table_Minor_Heading'] = "...";
+$Display_Array['Display_Items'] = array("ID", "Description");
+$Display_Array['Column_Width'] = array("50px", "*");
+$Display_Array['Item_Links'] = "";
+$Display_Array['New_Link_Reference'] = "";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `rec_assumptions` ";
+$Display_Array['MySQL_Filter'] = "WHERE `Project ID` = ".$Item_ID." AND (`Deleted Date` IS NULL OR `Deleted Date` = '".date("Y-m-d")."') ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_List_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['Table_Major_Heading'] = "RISK(S)";
+$Display_Array['Table_Minor_Heading'] = "...";
+$Display_Array['Display_Items'] = array("ID", "Contingency ID", "Health Risk", "Commercial Risk", "Completed Date", "Completed By", "");
+$Display_Array['Column_Width'] = array("50px", "100px", "100px", "100px", "100px", "200px", "*");
+$Display_Array['Item_Links'] = "REC_Risk.php";
+$Display_Array['New_Link_Reference'] = "Project ID";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `reg_risk-assessment` ";
+$Display_Array['MySQL_Filter'] = "WHERE `Project ID` = ".$Item_ID." AND (`Deleted Date` IS NULL OR `Deleted Date` = '".date("Y-m-d")."') ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_List_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+<?php
+
+$Display_Array = null;
+$Display_Array['ID'] = $Item_ID;
+$Display_Array['IS_Report'] = true;
+$Display_Array['New_Page_At_Print'] = false;
+$Display_Array['Table_Major_Heading'] = "OBSTACLE(S)";
+$Display_Array['Table_Minor_Heading'] = "...";
+$Display_Array['Display_Items'] = array("ID", "Description");
+$Display_Array['Column_Width'] = array("50px", "*");
+$Display_Array['Item_Links'] = "";
+$Display_Array['New_Link_Reference'] = "";
+
+$Display_Array['MySQL_Action'] = "SELECT * ";
+$Display_Array['MySQL_Table'] = "FROM `rec_obstacles` ";
+$Display_Array['MySQL_Filter'] = "WHERE `Project ID` = ".$Item_ID." AND (`Deleted Date` IS NULL OR `Deleted Date` = '".date("Y-m-d")."') ";
+$Display_Array['MySQL_Order'] = "";
+$Display_Array['MySQL_Limit'] = "";
+$Display_Array['MySQL_Offset'] = "";
+
+Dispaly_List_0001($Database_Connection, $Display_Array);
+
+?>
+
+
+</div>
+</body>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/Formats/Footer_Basic.php';?>
+
+</html> 
